b19bbd70b07e304a48ca610bb3a5b39323625111
manish96sinha <manish96sinha@gmail.com>
2024-04-08 00:02:05 +0530

    ldr_value_I2cdisplay

---
+++ Day12_01/Inc/adc.h
@@ -0,0 +1,19 @@
+/*
+ * adc.h
+ *
+ *  Created on: Apr 6, 2024
+ *      Author: manish
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "stm32f4xx.h"
+#include "stm32f407xx.h"
+
+void ADC_Init(void);
+uint16_t ADC_GetValue(void);
+
+
+
+#endif /* ADC_H_ */
